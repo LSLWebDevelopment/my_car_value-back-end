55b12e177efae350bed2f86a4cae13b0eb17628f
LSL Web Development <lslwebdevelopment@gmail.com>
2025-09-12 12:14:07 -0300

    Added the online route of the project

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:5173', 'https://my-car-value-nu.vercel.app'],
-    methods: ['POST', 'OPTIONS'],
+    methods: ['POST', 'GET', 'PATCH', 'DELETE', 'PUT', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
