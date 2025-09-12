679a0a701ac3819a6520f851e1c81ffa41bb700c
LSL Web Development <lslwebdevelopment@gmail.com>
2025-09-12 14:30:16 -0300

    Updated the url

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://my-car-value-nu.vercel.app'],
+    origin: [
+      'http://localhost:5173',
+      'https://mycarvalue-front-end-parcel.vercel.app/',
+    ],
     methods: ['POST', 'GET', 'PATCH', 'DELETE', 'PUT', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
