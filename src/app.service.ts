abf85c0a691dbd27605632b08b70a44d4390bf60
LSL Web Development <lslwebdevelopment@gmail.com>
2025-09-12 13:51:30 -0300

    changed the hello world message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'My Car Value!';
+    return "let's see if this is going to work now...";
   }
 }
