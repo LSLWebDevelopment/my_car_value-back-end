0d730ddde2ae21b926f73ea6e2897b183c741233
LSL Web Development <lslwebdevelopment@gmail.com>
2025-09-12 16:46:19 -0300

    made adjustments

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      'http://localhost:5173',
-      'https://mycarvalue-front-end-parcel.vercel.app',
+      'https://my-car-value-front-end.vercel.app', // your frontend
+      'http://localhost:5173', // local dev (Vite default)
     ],
-    methods: ['POST', 'GET', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT ?? 3000);
